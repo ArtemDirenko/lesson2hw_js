86464aeec35e9f8e4cd5d067a41799a3fce41c1d
“ArtemDirenko” <direnko28@gmail.com>
2022-11-07 15:36:39 +0200

    lesson2hw_js

---
+++ main.js
@@ -0,0 +1,86 @@
+let arr = [100, 'uk', 500, 4000, 'usa', 'art', 'war', 11, 50, true]
+console.log(arr)
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[3])
+console.log(arr[4])
+console.log(arr[5])
+console.log(arr[6])
+console.log(arr[7])
+console.log(arr[8])
+console.log(arr[9])
+
+
+
+let books ={title: 'HarryPotter', pageCount: 350, genre: 'fantasy' }
+console.log(books)
+
+let books2 ={
+    title: 'HarryPotter',
+    pageCount: 350,
+    genre: 'fantasy',
+    authors:[ 'name:rowling', 'age:46']}
+
+console.log(books2.authors)
+
+
+
+let userArray = [
+    {
+        name: 'anton',
+        username: 'qweghj',
+        password: 178
+    },
+    {
+        name: 'oly',
+        username: 'tghjh',
+        password: 1298
+    },
+    {
+        name: 'artem',
+        username: 'fjgjkhkm',
+        password: 1230
+    },
+    {
+        name: 'oleq',
+        username: 'ertyhj',
+        password: 1234
+    },
+    {
+        name: 'misha',
+        username: 'tohjkm',
+        password: 12345
+    },
+    {
+        name: 'katy',
+        username: 'klom',
+        password: 1236
+    },
+    {
+        name: 'koly',
+        username: 'klhkjtom',
+        password: 1237
+    },
+    {
+        name: 'pasha',
+        username: 'tomhjg',
+        password: 1238
+    },
+    {
+        name: 'ira',
+        username: 'tombgjbj',
+        password: 1239
+    },
+    {
+        name: 'masha',
+        username: 'tom',
+        password: 1231
+    },
+]
+console.log(userArray[0].password)
+console.log(userArray[1].password)
+console.log(userArray[2].password)
+console.log(userArray[3].password)
+console.log(userArray[4].password)
+console.log(userArray[5].password)
